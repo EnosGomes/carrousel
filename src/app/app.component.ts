7162fb64bd8dbd051a67ef88b64fc19805cb7a2b
enos <enoskratos@hotmail.com>
2024-05-23 22:18:46 -0300

    fazendo funcionar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgClass, NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+
+import { ChangeDetectionStrategy, OnInit } from '@angular/core';
+
+let nextId: number = 0;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,78 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'carrosel';
+  public componentId: string | undefined = `carousel-id-${nextId++}`;
+
+  public items: string[] = ['e', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  public currentPage: number = 0;
+
+  public itemMap: Map<number, string[]> | undefined;
+
+  public itemKeys: number[] | undefined;
+
+  public pageSize: number = 3;
+
+  public numberOfPages: number = 0;
+
+  public isNextDisabled: boolean = true;
+
+  public isPrevDisabled: boolean = true;
+
+  ngOnInit(): void {
+    this.createSliderGroups();
+    this.numberOfPages = Math.ceil(this.items.length / this.pageSize);
+    this.calcButtonStates();
+  }
+
+  public navToNext(): void {
+    if (this.currentPage >= this.numberOfPages - 1) {
+      this.reset();
+    } else {
+      this.currentPage = this.currentPage + 1;
+      this.navigateToGroup(this.currentPage);
+    }
+  }
+
+  public navToPrev(): void {
+    this.currentPage = this.currentPage - 1;
+    this.navigateToGroup(this.currentPage);
+  }
+
+  private navigateToGroup(groupId: number): void {
+    this.calcButtonStates();
+    const sliderGroup: Element | null = document.querySelector(
+      `#${this.componentId}-group-${groupId}`
+    );
+
+    sliderGroup?.scrollIntoView({
+      block: 'nearest',
+      inline: 'nearest',
+      behavior: 'smooth',
+    });
+  }
+
+  private createSliderGroups(): void {
+    this.itemMap = new Map();
+    let groupIndex: number = 0;
+
+    for (let i: number = 0; i < this.items.length; i += this.pageSize) {
+      const group: string[] = this.items.slice(i, i + this.pageSize);
+
+      this.itemMap.set(groupIndex, group);
+      groupIndex++;
+    }
+
+    this.itemKeys = Array.from(this.itemMap.keys());
+  }
+
+  private reset(): void {
+    this.currentPage = 0;
+    this.navigateToGroup(this.currentPage);
+  }
+
+  private calcButtonStates(): void {
+    this.isNextDisabled = this.currentPage + 1 >= this.numberOfPages;
+    this.isPrevDisabled = this.currentPage <= 0;
+  }
 }
